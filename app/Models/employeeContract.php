236e01332dc9e4815ea385b3d6507733b136e236
Rick <derickharon6@gmail.com>
2024-03-14 23:16:06 +0300

    backend-updates2

--- app/Models/employeeContract.php
+++ app/Models/employeeContract.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class employeeContract extends Model
 {
     use HasFactory;
+
+    protected $table = 'employee_contracts';
+    protected $primaryKey = 'No';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'No', 'employeeNo', 'employeeName', 'reasonForDecline', 'status', 'accepted',
+    ];
 }
